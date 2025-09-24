7d93c56b82b90eb751e5ed1edf8d84a1c47c2d18
startMrSnow <175dollarsnow@gmail.com>
2025-09-24 13:45:34 +0300

    Change: layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './components/home-page/home-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "home", component: HomePage },
+    {path: '', redirectTo: 'home', pathMatch: 'full' }
+];
